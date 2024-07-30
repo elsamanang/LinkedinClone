5dd8bd55b9081d0776fff6ca6ded23bf83a7cf98
elsa.m <manangelsa07@gmail.com>
2024-07-30 16:49:39 +0200

    animation

--- LinkedinClone/ViewModels/MessageModalViewModel.cs
+++ LinkedinClone/ViewModels/MessageModalViewModel.cs
@@ -1,5 +1,7 @@
 ﻿using CommunityToolkit.Maui.Views;
 using CommunityToolkit.Mvvm.Input;
+using Microsoft.Maui.Animations;
+using Microsoft.Maui.Controls.Internals;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,10 +12,37 @@ namespace LinkedinClone.ViewModels
 {
     public partial class MessageModalViewModel : BaseViewModel
     {
-        [RelayCommand]
-        public void HandleClose(Popup popup)
+        public MessageModalViewModel()
         {
+            var popup = Shell.Current.FindByName("MyPopup") as Popup;
+
+            if (popup != null)
+            {
+                popup.HorizontalOptions = Microsoft.Maui.Primitives.LayoutAlignment.End;
+                for (int i = 40; i > 0; i -= 40)
+                {
+                    popup.Content.TranslationX = i;
+                    HanldeDelay();
+                }
+            }
+        }
+
+
+        [RelayCommand]
+        public async Task HandleClose(Popup popup)
+        {
+            popup.HorizontalOptions = Microsoft.Maui.Primitives.LayoutAlignment.End;
+            for (int i = 40; i < 400; i += 40)
+            {
+                popup.Content.TranslationX = i;
+                await HanldeDelay();
+            }
             popup.Close();
+        }
+
+        public Task HanldeDelay()
+        {
+            return Task.Delay(1);
         }
     }
 }
