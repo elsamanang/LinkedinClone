25dbaf00c7352b8db52e427840daf5724c98172e
elsa.m <manangelsa07@gmail.com>
2024-08-09 16:21:13 +0200

    ading post

---
+++ LinkedinClone/Models/Post.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedinClone.Models
+{
+#nullable disable
+    public class Post
+    {
+        public string Id { get; set; }  
+        public string UserName { get; set; }
+        public string UserFunction { get; set; }
+        public string UserProfil { get; set; }
+        public int PostLike {  get; set; }
+        public int PostLove {  get; set; }
+        public int PostClap {  get; set; }
+        public int PostSmile {  get; set; }
+        public int PostSupport {  get; set; }
+        public int PostIdea {  get; set; }
+        public string Details { get; set; }
+        public string PostPicture { get; set; }
+        public DateTime DateCreation { get; set; }
+    }
+}
--- LinkedinClone/ViewModels/HomeViewModel.cs
+++ LinkedinClone/ViewModels/HomeViewModel.cs
@@ -1,8 +1,11 @@
 ﻿using CommunityToolkit.Maui.Core;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using LinkedinClone.Models;
+using LinkedinClone.Utiles;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,6 +17,9 @@ namespace LinkedinClone.ViewModels
         [ObservableProperty]
         [NotifyPropertyChangedFor(nameof(IsNotSearching))]
         private bool _isSearching;
+
+        [ObservableProperty]
+        public ObservableCollection<Post> _posts;
         public bool IsNotSearching => !IsSearching;
         private IPopupService _popupService;
 
@@ -22,6 +28,8 @@ namespace LinkedinClone.ViewModels
             _popupService = popupService;
             IsSearching = false;
             Title = "My Home Page";
+            var data = MockGenerator.GeneratePosts(20);
+            Posts = new ObservableCollection<Post>(data.OrderByDescending(p => p.DateCreation));
         }
 
         [RelayCommand]
